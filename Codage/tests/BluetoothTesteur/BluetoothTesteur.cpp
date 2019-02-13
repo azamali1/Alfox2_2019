7837613b2c44aad7335fd7108f140ca1179cd4fd
azamali1 <azamali@btslivh.eu>
2019-02-13 17:34:02 +0100

    codes de test bluetooth

---
+++ Codage/tests/BluetoothTesteur/BluetoothTesteur.cpp
@@ -0,0 +1,46 @@
+#include "../../src/Bluetooth.h"
+
+
+Bluetooth* bluetooth;
+char c = ' ';
+boolean NL = true;
+
+void setup() {
+  Serial.begin(9600);
+  delay(2500);
+  bluetooth = new Bluetooth(PINALIM, PINEN);
+  Serial.println("Test de la classe Bluetooth");
+  //bluetooth->connexion("2017,11,7030A"); // OBD2 bleu
+  //bluetooth->connexion("780C,B8,46F54"); // PC Commenge simulateur
+  bluetooth->connexion("E84E,84,CCF54A"); //Portable ZAMALI
+  delay(2000);
+  Serial.println(bluetooth->isActif());
+
+}
+
+void loop()
+{
+    if (bluetooth->getLiaisonBT()->available())
+    {
+      c = bluetooth->getLiaisonBT()->read();
+      Serial.write(c);
+    }
+
+    // Read from the Serial Monitor and send to the Bluetooth module
+    if (Serial.available())
+    {
+      c = Serial.read();
+      bluetooth->getLiaisonBT()->write(c);
+      if (NL) { Serial.print("\r\n>");  NL = false; }
+      Serial.write(c);
+      if (c==10)
+      {
+        NL = true;
+      }
+  }
+}
+
+void SERCOM3_Handler()
+{
+  bluetooth->getLiaisonBT()->IrqHandler();
+}
