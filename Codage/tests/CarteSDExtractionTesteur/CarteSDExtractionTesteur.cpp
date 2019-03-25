e71a2f7be0f75283d373d5a0a70b9243ad3385f3
azamali1 <azamali@btslivh.eu>
2019-03-25 09:50:36 +0100

    Classe Bluetooth et tests DonneesTR, CarteSD

---
+++ Codage/tests/CarteSDExtractionTesteur/CarteSDExtractionTesteur.cpp
@@ -0,0 +1,49 @@
+#include "../../src/CarteSD.h"
+#include "../../src/DonneesTR.h"
+#include <SD.h>
+#include <SPI.h>
+
+CarteSD* carteSD;
+DonneesTR* donneesTR;
+int conso = 0;
+enum MODE {creation, ecriture, lecture , supression , taille, extraction};
+
+String nomDuFichier = "180606.TXT"; // nom du fichier qui va être créé, lue, et supprimer
+String commande=""; // chaine qui va stocker la commande demander
+char c;
+
+void setup() {
+  Serial.begin(9600); //debut de la connection au port serie
+  delay(10000);
+  carteSD = new CarteSD(); // instanciation de carteSD
+  donneesTR = new DonneesTR(); // instanciation de donneesTR
+  delay(1000);
+  Serial.println("Initialisation de la carte SD OK");
+
+
+
+
+}
+
+void loop() {
+
+  if (Serial.available()) { // tant que des caractères sont en attente d'être lus
+        c = Serial.read(); // on lit le caractère
+        commande = commande +c;
+        Serial.flush();
+        Serial.println("caractère saisi : "+c);
+
+    }
+    //delay(1000);
+    if(commande=="extraction"){
+      Serial.println("Lancement de l'extration");
+      carteSD->extractionRacine();
+      commande = "";
+    }
+    else{
+      commande = "";
+    }
+
+}
+
+
--- Codage/tests/LedTriTesteur/build/MKMK/-.d
+++ Codage/tests/LedTriTesteur/build/MKMK/-.d
@@ -1,2 +1,2 @@
-.sc2884409523877783201.o: \
- /home/snir2g2/Bureau/Alfox2_2019/Codage/tests/LedTriTesteur/.sc2884409523877783201.cpp
+.sc4518456025369013381.o: \
+ /home/snir2g2/Bureau/Alfox2_2019/Codage/tests/LedTriTesteur/.sc4518456025369013381.cpp
